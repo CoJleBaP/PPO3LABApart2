551a0c07166b6c25835c0064f93992eac391527f
CoJleBaP <151513830+CoJleBaP@users.noreply.github.com>
2023-12-28 18:11:16 +0300

    Add files via upload

---
+++ 123.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft Visual Studio \2019\Common7\IDE\
+VCExpress.exe" Beliasov_Repository.sln /build Debug
+"E:\doxygen\bin\doxygen.exe" myConfig
+"E:\Inno Setup 6\ISCC.exe" build.iss
